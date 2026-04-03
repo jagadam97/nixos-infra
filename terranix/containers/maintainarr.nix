c5911f6711aa5b9d00105362653981f4bdedcc78
J Dinesh Reddy <dinesh.reddy@juspay.in>
2026-04-04 03:46:28 +0530

    feat: update storage configuration for maintainarr and qbittorrent

--- terranix/containers/maintainarr.nix
+++ terranix/containers/maintainarr.nix
@@ -6,8 +6,8 @@
 
   cores = 2;
   memory = 2048;
-  storage = "bx500";
-  template_file_storage = "hd4000";
+  # storage = "local-lvm";  # Use default
+  template_file_storage = "bx500";
 
   # Mount points
   # Config persisted on bx500; media storage on bx1000 (ssd) and hd4000 (hdd)
