2da30182cad965d2cdf595f18503ceb2977abcd1
J Dinesh Reddy <dinesh.reddy@juspay.in>
2026-04-04 02:14:53 +0530

    maintainer

--- terranix/containers/maintainarr.nix
+++ terranix/containers/maintainarr.nix
@@ -10,18 +10,18 @@
   template_file_storage = "hd4000";
 
   # Mount points
-  # Config persisted on bx500; media storage on ssd and hdd
+  # Config persisted on bx500; media storage on bx1000 (ssd) and hd4000 (hdd)
   mount_points = [
     {
       volume = "/mnt/pve/bx500/maintainarr";
       path = "/config";
     }
     {
-      volume = "/mnt/pve/ssd";
+      volume = "/mnt/pve/bx1000";
       path = "/mnt/ssd";
     }
     {
-      volume = "/mnt/pve/hdd";
+      volume = "/mnt/pve/hd4000";
       path = "/mnt/hdd";
     }
   ];
